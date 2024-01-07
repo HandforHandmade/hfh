f5ba6823684f3f6d0a4e77f86c324573c2ae3101
Pinank Soni <pinank@miwebsolution.com>
2024-01-07 14:13:58 +0530

    new changes

--- include/header.php
+++ include/header.php
@@ -14,9 +14,11 @@ include("DB/connection.php");
             <div class="float-right">
                 <ul class="h_social list_style">
                     <li><a href="https://www.facebook.com/HandForHandmade/"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="https://twitter.com/i/flow/login?redirect_after_login=%2FHandForHandmade"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="https://twitter.com/i/flow/login?redirect_after_login=%2FHandForHandmade"><i
+                                class="fa fa-twitter"></i></a></li>
                     <!-- <li><a href="#"><i class="fa fa-google-plus"></i></a></li> -->
-                    <li><a href="https://www.linkedin.com/company/a-hand-for-handmade/"><i class="fa fa-linkedin"></i></a></li>
+                    <li><a href="https://www.linkedin.com/company/a-hand-for-handmade/"><i
+                                class="fa fa-linkedin"></i></a></li>
                 </ul>
                 <!-- <ul class="h_search list_style">
                     <li class="shop_cart"><a href="#"><i class="lnr lnr-cart"></i></a></li>
@@ -34,20 +36,20 @@ include("DB/connection.php");
                     <img src="img/logo-3.png" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
-                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                        aria-expanded="false" aria-label="Toggle navigation">
-							<span class="my_toggle_menu">
-                            	<span></span>
-                            	<span></span>
-                            	<span></span>
-                            </span>
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="my_toggle_menu">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
 
                         <li class="dropdown submenu">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                               aria-haspopup="true" aria-expanded="false"> About </a>
+                                aria-haspopup="true" aria-expanded="false"> About </a>
                             <ul class="dropdown-menu">
                                 <li><a href="hand-for-handmade.php"> Hand For Handmade (HFH) </a></li>
                                 <li><a href="team.php?id=4"> Founder And Directors </a></li>
@@ -57,11 +59,19 @@ include("DB/connection.php");
                                 <li><a href="initiatives.php"> Initiatives </a></li>
                             </ul>
                         </li>
-
-
                         <li class="dropdown submenu">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                               aria-haspopup="true" aria-expanded="false"> Our Activities </a>
+                                aria-haspopup="true" aria-expanded="false"> Hamari Virasat </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="./pdf/Hamari Virasat_Pitch Deck.pdf" target="_blank"> About </a></li>
+                                <li><a href="./pdf/brochure.pdf" target="_blank"> Brochure </a></li>
+                                <li><a href="mumbai-launch.php"> Mumbai launch </a></li>
+
+                            </ul>
+                        </li>
+                        <li class="dropdown submenu">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                                aria-haspopup="true" aria-expanded="false"> Our Activities </a>
                             <ul class="dropdown-menu" id="activityDropDown">
 
                                 <?php
@@ -70,18 +80,16 @@ include("DB/connection.php");
 
                                 while ($RowData = mysqli_fetch_assoc($eventCategoryResult)) {
                                     $id = $RowData['id'];
-                                    ?>
+                                ?>
 
-                                    <li><a href="event-list.php?id=<?php echo $id;?>"> <?php echo $RowData['category_name']; ?> </a></li>
+                                <li><a href="event-list.php?id=<?php echo $id; ?>">
+                                        <?php echo $RowData['category_name']; ?> </a></li>
                                 <?php } ?>
-                               <!-- <li><a href="handmadekatha.php"> HandmadeKatha </a></li>-->
-
                             </ul>
                         </li>
-
                         <li class="dropdown submenu">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                               aria-haspopup="true" aria-expanded="false"> Members </a>
+                                aria-haspopup="true" aria-expanded="false"> Members </a>
                             <ul class="dropdown-menu">
                                 <li><a href="membership-benefits.php"> Membership Benefits </a></li>
                                 <li><a href="view-members.php"> View Members </a></li>
@@ -89,32 +97,12 @@ include("DB/connection.php");
                             </ul>
                         </li>
 
-
-                        <!-- <li class="dropdown submenu active">
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Home</a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#"> LINK </a></li>
-                                <li><a href="#"> LINK </a></li>
-                                <li><a href="#"> LINK </a></li>
-                                <li><a href="#"> LINK </a></li>
-                            </ul>
-                        </li>
-                        <li><a href="cake.html">Our Cakes</a></li>
-                        <li><a href="menu.html">Menu</a></li>
-                        <li class="dropdown submenu">
-                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">About Us</a>
-                            <ul class="dropdown-menu">
-                                <li><a href="about-us.html">About Us</a></li>
-                                <li><a href="our-team.html">Our Chefs</a></li>
-                                <li><a href="testimonials.html">Testimonials</a></li>
-                            </ul>
-                        </li> -->
                     </ul>
                     <ul class="navbar-nav justify-content-end">
 
                         <li class="dropdown submenu">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                               aria-haspopup="true" aria-expanded="false"> Explore HFH </a>
+                                aria-haspopup="true" aria-expanded="false"> Explore HFH </a>
                             <ul class="dropdown-menu">
                                 <li><a href="resources.php"> Resources </a></li>
                                 <li><a href="be-a-donor.php"> Be a Donor </a></li>
@@ -126,23 +114,23 @@ include("DB/connection.php");
                         <li><a href="membership.php"> Membership </a></li>
                         <?php
                         if (isset($_SESSION) && isset($_SESSION['loginFlag']) && $_SESSION['loginFlag'] === 'FlagTrue') { ?>
-                            <li class="dropdown submenu">
-                                <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
-                                   aria-haspopup="true" aria-expanded="false"> <?php echo $_SESSION['name']; ?> </a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="profile.php">Profile</a></li>
-                                    <li><a href="portfolio.php"> Portfolio </a></li>
-                                    <li><a href="catalogue.php"> catalogue </a></li>
+                        <li class="dropdown submenu">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                                aria-haspopup="true" aria-expanded="false"> <?php echo $_SESSION['name']; ?> </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="profile.php">Profile</a></li>
+                                <li><a href="portfolio.php"> Portfolio </a></li>
+                                <li><a href="catalogue.php"> catalogue </a></li>
 
-                                </ul>
-                            </li>
-                            <li><a href="logout.php">Logout</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="logout.php">Logout</a></li>
 
                         <?php } else {
-                            ?>
-                            <li><a href="signin.php"> Sign In / Sign up </a></li>
+                        ?>
+                        <li><a href="signin.php"> Sign In / Sign up </a></li>
                         <?php } ?>
-                        <!-- <li class="dropdown submenu">
+                        <!-- <li class="dropdown submenu    ">
                             <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
                             <ul class="dropdown-menu">
                                 <li><a href="service.html">Services</a></li>
