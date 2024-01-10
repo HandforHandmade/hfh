8f0adb63b084d73f36cd72ff9bb50480c0c1c477
Pinank Soni <pinank@miwebsolution.com>
2024-01-10 14:07:05 +0530

    logo display issue in mobile view

--- include/header.php
+++ include/header.php
@@ -31,13 +31,12 @@ include("DB/connection.php");
         <div class="container">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <a class="navbar-brand" href="index.php">
-                    <!-- <img src="img/logo.png" alt="">
-                    <img src="img/logo-2.png" alt="">
-                    <img src="img/logo-3.png" alt=""> -->
+
+                    <img src="img/logo.png" alt="">
+                    <img src="img/logo-3.png" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="my_toggle_menu">
-                        <span></span>
                         <span></span>
                         <span></span>
                     </span>
